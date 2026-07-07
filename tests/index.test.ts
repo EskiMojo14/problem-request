2a49b501de4c5f2466f8531c721191a03c532745
Ben Durrant <ben.durrant@marketdojo.com>
2026-07-07 22:40:33 +0100

    create ProblemResponse class to handle RFC 9457 problem details

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,27 @@
-import { expect, test } from "vite-plus/test";
-import { fn } from "../src/index.ts";
+import { expect, it, describe } from "vite-plus/test";
+import { ProblemResponse, type LooseProblemDetails } from "../src/index.ts";
 
-test("fn", () => {
-  expect(fn()).toBe("Hello, tsdown!");
+describe("ProblemResponse", () => {
+  const problemDetails: LooseProblemDetails = {
+    type: "https://example.com/probs/out-of-credit",
+    title: "You do not have enough credit.",
+    detail: "Your current balance is 30, but that costs 50.",
+    instance: "/account/12345/msgs/abc",
+    accounts: ["/account/12345", "/account/67890"],
+  };
+  it("should create a ProblemResponse with default status", async () => {
+    const response = ProblemResponse.problem(problemDetails);
+    expect(response).toBeInstanceOf(Response);
+    expect(response).toBeInstanceOf(ProblemResponse);
+    expect(response.status).toBe(500);
+    expect(response.headers.get("Content-Type")).toBe("application/problem+json");
+    await expect(response.json()).resolves.toEqual(problemDetails);
+  });
+  it("should create a ProblemResponse with custom status", () => {
+    const response = ProblemResponse.problem({
+      ...problemDetails,
+      status: 403,
+    });
+    expect(response.status).toBe(403);
+  });
 });
